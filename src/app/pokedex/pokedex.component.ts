6c23c5e999de93c4f5a72eeecd4028ef3e140e0a
GuiKazuo1003 <guilhermeiama@gmail.com>
2023-04-30 20:22:09 -0300

    primeiro_commit

---
+++ src/app/pokedex/pokedex.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { PokemonService } from '../pokemon.service';
+import { Pokemon } from '../pokemon';
+
+@Component({
+  selector: 'app-pokedex',
+  templateUrl: './pokedex.component.html',
+  styleUrls: ['./pokedex.component.css']
+})
+export class PokedexComponent {
+  pokemon: Pokemon = {} as Pokemon;
+  pokemonId: number = 1;
+  MAX: number = 1010;
+
+  constructor(private pokemonService: PokemonService) { }
+
+  ngOnInit(): void{
+    this.loadPokemon();
+  }
+
+  loadPokemon(): void{
+    this.pokemonService.getPokemon(this.pokemonId).subscribe(
+      {
+        next : pokemon => this.pokemon = pokemon
+      }
+    );
+  }
+  getFotoPokemon(){
+    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${this.pokemonId}.png`;
+}
+  ProximoPokemon(){
+    if (this.pokemonId === this.MAX){
+      this.pokemonId = 1;
+    }
+    else{
+      this.pokemonId++;
+    }
+    this.loadPokemon();
+  }
+
+  AnteriorPokemon(){
+    if (this.pokemonId === this.MAX){
+      this.pokemonId = 1;
+    }
+    else{
+      this.pokemonId--;
+    }
+    this.loadPokemon();
+  }
+}
